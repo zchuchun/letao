e5db95ffdd78d7585588540e5e15c1e18cb01de3
zchuchun <2275953401@qq.com>
2018-11-21 15:48:14 +0800

    登录页及其功能完成

---
+++ public/back/js/common.js
@@ -0,0 +1,13 @@
+
+
+// 进度条
+$(function(){
+    $(document).ajaxStart(function(){
+        NProgress.start();
+    })
+    $(document).ajaxStop(function(){
+        setTimeout(function(){
+            NProgress.done();
+        },2000)
+    })
+})
---
+++ public/back02/js/login.js
@@ -0,0 +1,80 @@
+
+$(function(){
+    //使用表单校验插件
+$("#form").bootstrapValidator({
+ 
+    // 指定校验时的图标显示，默认是bootstrap风格
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+  
+    // 指定校验字段
+    fields: {
+      //校验用户名，对应name表单的name属性
+      username: {
+        validators: {
+          //不能为空
+          notEmpty: {
+            message: '用户名不能为空'
+          },
+          //长度校验
+          stringLength: {
+            min: 2,
+            max: 6,
+            message: '用户名长度必须在2到6之间'
+          },
+          callback:{
+              message:"用户名不存在"
+          }
+         
+        }
+      },
+
+      password:{
+          validators:{
+              notEmpty:{
+                  message:"密码不能为空"
+              },
+              stringLength:{
+                min:6,
+                max:12,
+                message:"密码长度必须在6到12之间"
+              },
+              callback:{
+                  message:"密码错误"
+              }
+          }
+      }
+    }
+  
+  });
+
+
+  $("#form").on('success.form.bv', function (e) {
+    e.preventDefault();
+    //使用ajax提交逻辑
+
+    $.ajax({
+        type:"post",
+        url:"/employee/employeeLogin",
+        dataType:"json",
+        data:$("#form").serialize(),
+        success:function(info){
+            console.log(info);
+            if(info.success){
+                location.href = "index.html";
+            }
+            if(info.error === 1000){
+                // alert("用户名不存在");
+                $("#form").data("bootstrapValidator").updateStatus("username", "INVALID", "callback")
+            }
+            if(info.error ===1001){
+                // alert("密码错误");
+                $("#form").data("bootstrapValidator").updateStatus("password","INVALID","callback");
+            }
+        }
+    })
+});
+})
