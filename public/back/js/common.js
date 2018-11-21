08adfa7836225d3bf2b30313b2b059e9b7abb84c
zchuchun <2275953401@qq.com>
2018-11-21 21:01:37 +0800

    首页及其功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -11,3 +11,40 @@ $(function(){
         },2000)
     })
 })
+
+
+// 公用的功能
+
+$(".category").click(function(){
+    $(this).next().stop().slideToggle();
+})
+
+// 左侧菜单栏切换
+
+$(".top .left").click(function(){
+    $('.lt_aside').toggleClass("hidemenu");
+    $('.top').toggleClass("hidemenu");
+    $('.main').toggleClass("hidemenu");  
+})
+
+
+//点击退出
+
+$(".top .right").click(function(){
+    $("#myModal").modal("show");
+})
+
+
+$(".loginout").click(function(){
+    $.ajax({
+        type:'get',
+        url:'/employee/employeeLogout',
+        dataType:"json",
+        success:function(info){
+            console.log(info);
+            if(info.success){
+                location.href = "login.html";
+            }
+        }
+    })
+})
