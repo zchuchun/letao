5f68b5da892fa6860123312e91eeca92012ef2bd
zchuchun <2275953401@qq.com>
2018-11-23 11:13:54 +0800

    二级分类表单验证完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -58,6 +58,71 @@ $(function(){
 $(".dropdown-menu").on("click",'a',function(){
     var txt = $(this).text();
     $('.cate-text').text(txt);
+
+    // 获取a中的自定义属性存储的id赋值给隐藏域，用于提交
+    var id = $(this).data("id");
+    $('[name="categoryId"]').val(id);
+
+    //手动将隐藏域的校验状态改为成功
+    $('#form').data("bootstrapValidator").updateStatus('categoryId','VALID');
+
+    
 })
+    // 调用fileUpload方法，发送文件上传请求
+   $("#fileupload").fileupload({
+       dataType:"json",
+       done:function(e,data){
+        //    console.log(data);
+
+        var result = data.result;
+        var picUrl = result.picAddr;
+
+        $("#imgBox img").attr("src",picUrl);
+        $('[name="brandLogo"]').val(picUrl);
+
+        $('#form').data("bootstrapValidator").updateStatus("brandLogo","VALID");
+
+       }
+   })
+
+
+    // 添加表单校验
+    $("#form").bootstrapValidator({
+        //设置排除项
+        excluded:[],
+        // 配置小图标
+    feedbackIcons: {
+        valid: 'glyphicon glyphicon-ok',   // 校验成功
+        invalid: 'glyphicon glyphicon-remove',   // 校验失败
+        validating: 'glyphicon glyphicon-refresh'  // 校验中
+      },
+
+    //   配置校验字段
+
+    fields:{
+        categoryId:{
+            validators:{
+                notEmpty:{
+                    message:'请选择一级分类'
+                }
+            }
+        },
+        brandName:{
+            validators:{
+                notEmpty:{
+                    message:'请输入二级分类'
+                }
+            }
+        },
+        brandLogo:{
+            validators:{
+                notEmpty:{
+                    message:"请上传图片"
+                }
+            }
+        }
+    }
+    })
+
 
 })
